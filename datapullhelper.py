cda3d274c03eb26e0ff7842e448df9ced729c363
HiHi3690 <duimstrajakson@gmail.com>
2025-03-07 23:13:45 -0500

    rewrite for data grabbing, new sample data

--- datapullhelper.py
+++ datapullhelper.py
@@ -2,6 +2,8 @@ import pybaseball as pyb
 import pandas as pd
 import threading as th
 import os
+from time import sleep
+from timeit import default_timer
 
 """
 Season Dates:
@@ -16,31 +18,7 @@ All-encompassing dates:
 Feb 14 - Dec 25 (these are arbitrary)
 """
 
-def get_pitcher_info():
-    data = pyb.pitching_stats(2022, end_season=None, league='all', qual=1, ind=0)[["IDfg"]]
-    info = pyb.playerid_reverse_lookup(data["IDfg"].values, "fangraphs").sort_values(by="name_last")
-    info.to_excel("data/pitcher_data/pitcherinfo.xlsx", index=False)
-
-def cull_data(df: pd.DataFrame, cull_vars: [str]) -> pd.DataFrame:
-    if cull_vars == []:
-        return df
-    else:
-        return df[cull_vars]
-
-def get_data(lookup: dict, season:str, id: int, toFile: bool = False, cull_vars: [str] = []) -> pd.DataFrame:
-    data = cull_data(pyb.statcast_pitcher(season[0], season[1], id), cull_vars)
-    path = "data/pitcher_data/" + season[0][:4] + "/"+lookup[id] + ".xlsx"
-    if toFile:        
-        data.to_excel(path)
-    return data
-
-def get_all_data_in_season(lookup: dict, season:[str,str], pitcher_ids: [int], toFile: bool = False, cull_vars: [str] = []) -> pd.DataFrame:
-    for id in pitcher_ids:
-            get_data(lookup, season, id, toFile, cull_vars)
-
-def get_all_data(lookup: dict, season_list: [[str, str]], pitcher_ids: [int], toFile: bool = False, cull_vars: [str] = []) -> pd.DataFrame:
-    for season in season_list:
-        th.Thread(target=get_all_data_in_season, args=(lookup, season, pitcher_ids, toFile, cull_vars)).start()
+#data importing utils
 
 def get_all_files_in_directory(dir_path: str) -> [str]:
     file_list = []
@@ -56,3 +34,56 @@ def get_all_files() -> {str : [str]}:
         yr_path = "data/pitcher_data/" + yr
         data.update({yr : get_all_files_in_directory(yr_path)})
     return data
+
+# data api grab utils
+
+def cull_data(df: pd.DataFrame, cull_vars: [str]) -> pd.DataFrame:
+    if cull_vars == []:
+        return df
+    else:
+        return df[cull_vars]
+
+def get_lookup(year: int, to_file: bool = True) -> pd.DataFrame:
+    if to_file:
+        ids = pyb.pitching_stats(year, end_season=None, league="all", qual=1, ind=0)[["IDfg"]]
+        info = pyb.playerid_reverse_lookup(ids["IDfg"].values, "fangraphs").sort_values(by="name_last")
+        info.to_excel(f"data/pitcher_data/{str(year)}/{str(year)}_pitcher_metadata.xlsx")
+        print("wrote metadata file")
+        return info[["key_mlbam", "name_last", "name_first"]]
+    else:
+        info = pd.read_excel(f"data/pitcher_data/{str(year)}/{str(year)}_pitcher_metadata.xlsx")
+        return info[["key_mlbam", "name_last", "name_first"]]
+
+def get_all_data(season_list: [[str, str]], to_file: bool, cull_vars: [str] = []) -> None:
+    for season in season_list:
+        th.Thread(target=get_season_data, args=(season, to_file, cull_vars)).start() # split workers to each year
+
+def get_season_data(season: [str, str], to_file: bool, cull_vars: [str] = []) -> None:
+    # get look up table for names/ids
+    year = int(season[0][:4])
+    pitcher_info = get_lookup(year, True) # SWITCH TO TRUE IF YOU WANT TO REGEN DATA
+    pitcher_ids = pitcher_info["key_mlbam"]
+    pitcher_name= pitcher_info["name_last"] + "_" + pitcher_info["name_first"]
+    lookup = {pitcher_ids[i] : pitcher_name[i] for i in range(len(pitcher_ids))}
+    for id in pitcher_ids.head():
+        th.Thread(target=get_pitcher_data, args=(season, id, lookup, True, cull_vars)).start() #split off into per query workers
+        sleep(0.5) #to prevent rate limit
+
+def get_pitcher_data(season: [str, str], id: int, lookup: dict, to_file: bool = True, cull_vars: [str] = []):
+    data = cull_data(pyb.statcast_pitcher(season[0], season[1], id), cull_vars)
+    path = f"data/pitcher_data/{season[0][:4]}/{lookup[id]}.xlsx"
+    if to_file:
+        data.to_excel(path)
+        print(f"wrote {path}")
+
+"""
+# get the preliminary metadata loaded into variables:
+
+pitcher_info = pd.read_excel("data/pitcherinfo.xlsx")
+
+pitcher_ids = pitcher_info["key_mlbam"]
+pitcher_name= pitcher_info["name_last"] + "_" + pitcher_info["name_first"]
+
+# lookup table that converts MLBAM id's to last_first name
+lookup = {pitcher_ids[i] : pitcher_name[i] for i in range(len(pitcher_ids))}
+"""
--- main.py
+++ main.py
@@ -8,35 +8,10 @@ import datacalchelper as dc
 # init pitcher list, only need to do once, so commented for now to save runtime
 # dp.get_pitcher_info()
 
-# get the preliminary metadata loaded into variables:
-
-pitcher_info = pd.read_excel("data/pitcherinfo.xlsx")
-
-pitcher_ids = pitcher_info["key_mlbam"]
-pitcher_name= pitcher_info["name_last"] + "_" + pitcher_info["name_first"]
-
-# lookup table that converts MLBAM id's to last_first name
-lookup = {pitcher_ids[i] : pitcher_name[i] for i in range(len(pitcher_ids))}
-
 # list of seasons to pull data for
 season_list = [[j + "-02-14", j+ "-12-25"] for j in [str(i) for i in range(2021, 2025)]]
 # list of variables to keep (cull_vars)
 cull_vars = "game_date events description zone des type hit_location bb_type balls strikes on_3b on_2b on_1b outs_when_up inning inning_topbot at_bat_number pitch_number home_score away_score".split(" ")
 
-# example of how to get data for a single pitcher
-# data = dp.get_data(lookup, [["2021-02-14", "2021-12-25"]], 521230, False, c-ull_vars)
-
-# example of how to get data for all pitchers in a season
-# data = dp.get_all_data(lookup, season_list, pitcher_ids, True, cull_vars)
-
-# example of how to get sheet from dir to var
-data = pd.read_excel("data/pitcher_data/2021/akin_keegan.xlsx", sheet_name="Sheet1")
-
-# get all files in 2021 directory
-# dp.get_all_files_in_directory("data/pitcher_data/2021")
-
-# get all data file names
-# dp.get_all_files()
-
-
-print(dc.get_chase_percent_season(data))
+# RUN ONLY WHEN NEED TO REGEN DATA
+dp.get_all_data(season_list=season_list, to_file=True, cull_vars=cull_vars)
